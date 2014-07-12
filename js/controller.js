d45379f52c30c980c57cc6ee6211d35311da8057
Charles Naut <cpn585@gmail.com>
2014-07-12 08:52:21 -0700

    Setup basic search

--- js/controller.js
+++ js/controller.js
@@ -1,16 +1,22 @@
 angular.module('controllers', [])
-.controller('DashboardCtrl', function($scope, $location, LibraryService) {
-  $scope.items = [
-    { image: "books.jpg", title: "A bunch of books"},
-    { image: "coat.jpg", title: "Nice coat" },
-    { image: "bike.jpg", title: "Fixie Bike" },
-    { image: "dvd.jpg", title: "DVD"}
-  ];
+.controller('MainCtrl', function($scope, $rootScope, LibraryService) {
+  $scope.search = function() {
+    LibraryService.filterItems($scope.query);
+    $rootScope.$broadcast('items-changed');
+  };
+})
+
+.controller('DashboardCtrl', function($scope, $rootScope, $location, LibraryService) {
+  $scope.items = LibraryService.getItems();
+
+  $rootScope.$on('items-changed', function(event, args) {
+    $scope.items = LibraryService.getItems();
+  });
 
   $scope.checkout = function(item) {
     LibraryService.addToCart(item)
     $location.path("/checkout")
-  }
+  };
 })
 
 .controller('CheckoutCtrl', function($scope, LibraryService) {
--- js/service.js
+++ js/service.js
@@ -1,13 +1,27 @@
 angular.module('LibraryService', [])
-.factory('LibraryService', function($http) {
+.factory('LibraryService', function($http, $filter) {
   var cart = [];
+  var items = [
+    { image: "books.jpg", title: "A bunch of books"},
+    { image: "coat.jpg", title: "Nice coat" },
+    { image: "bike.jpg", title: "Fixie Bike" },
+    { image: "dvd.jpg", title: "DVD"},
+    { image: "crockpot.jpg", title: "Crockpot"},
+    { image: "tent.jpg", title: "Tent"}
+  ];
 
   var addToCart = function(item) {
   	cart.push(item);
-  }
+  };
+
+  var filterItems = function(query) {
+	items = $filter('filter')(items, { title: query });
+  };
 
   return {
   	addToCart: addToCart,
-  	getCart: function() { return cart }
+  	getCart: function() { return cart },
+  	getItems: function() { return items },
+  	filterItems: filterItems
   }
 });
