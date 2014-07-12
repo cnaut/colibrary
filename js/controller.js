34e14247df55fe46651f766d13529963a386f676
Charles Naut <cpn585@gmail.com>
2014-07-12 05:08:03 -0700

    Add some products

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,10 @@
 angular.module('controllers', [])
-.controller('DashboardCtrl', function($scope) {
-  
+.controller('DashboardCtrl', function($scope, $location) {
+  $scope.checkout = function() {
+    $location.path("/checkout")
+  }
 })
 
 .controller('CheckoutCtrl', function($scope) {
-  
+
 });
